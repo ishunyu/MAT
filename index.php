364648289d5e348cd43e82cc47b7183774ecb53f
Shun Yu <zelox991@gmail.com>
2012-02-24 20:10:50 -0800

    New Files

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+$s = "This is awesome!";
+
+?>
+
+<html>
+  <head>
+    <link rel="stylesheet" type="text/css" href="styles/style.css">
+  </head>
+  
+  <body>
+    <table align="center" class="main">
+      <tr>
+        <td><center>Gene Mutation Page</center></td>
+      </tr>
+    </table>
+  </body>
+</html>
+
+
+<?php
+
+
+?>
