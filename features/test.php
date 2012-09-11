330943e5b118fd505b95108305b417415c1eab20
Shun Yu <zelox991@gmail.com>
2012-09-11 14:19:16 -0700

    new test.php

--- features/test.php
+++ features/test.php
@@ -1,17 +1,6 @@
 <?
 require_once '../db/connectdb.php';
-
-$features_q = array(
-  "max_id"=> 6,
-  "0" => "m7G Cap",
-  "1" => "promoter",
-  "2" => "5'UTR",
-  "3" => "Exon",
-  "4" => "Intron",
-  "5" => "3'UTR",
-  "6" => "Poly(A) tail"
-);
-$features = mysql_escape_string(json_encode($features_q));  // Turns json into associative array
+require_once '../classes/FEATURES.php';
 
 $features_q = "update shunyu_genes set features = '$features'";
 mysql_query($features_q);
